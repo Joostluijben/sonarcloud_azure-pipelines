28394cec80e6ea4f13180dd2635591ab86530911
Joost <joost.luijben@student.hu.nl>
2020-07-03 14:58:32 +0200

    First commit

---
+++ src/main/java/joost/luijben/sonarcloud_azurepipelines/SonarcloudAzurePipelinesApplication.java
@@ -0,0 +1,13 @@
+package joost.luijben.sonarcloud_azurepipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarcloudAzurePipelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarcloudAzurePipelinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joost/luijben/sonarcloud_azurepipelines/SonarcloudAzurePipelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package joost.luijben.sonarcloud_azurepipelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarcloudAzurePipelinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
